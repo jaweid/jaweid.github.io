aa07bd8c456cf710f5b1084cf989dff380d8adf2
jiawei <1403360383@qq.com>
2020-11-02 19:44:50 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '技术积累', link: '/articles/' },
+            { text: '关于我', link: '/aboutme/' },
+            { text: '架构之路', link: '/archite/' },
+            { text: '掘金', link: 'https://juejin.im/' },
+            {
+                text: '语言',
+                items: [
+                    { text: '中文', link: '/language/chinese/' },
+                    { text: 'Japanese', link: '/language/japanese/' }
+                ]
+            }
+        ],
+        sidebar: {
+            '/articles/': [
+                '',
+                'js',
+            ],
+            '/archite/': [
+                '',
+                'knows'
+            ]
+        },
+        lastUpdated: 'Last Updated', // string | boolean
+        smoothScroll: true
+    },
+}
